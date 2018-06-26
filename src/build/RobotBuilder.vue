eb6712229ee9d6b947e051d823fc69a182363c35
Omar Silva <omarsilva@outlook.com>
2018-06-26 18:39:47 -0500

    Binding attributes to data

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,43 +1,50 @@
 <template>
-    <div>
-        <div class="top-row">
-            <div class="top part">
-                <img src="./images/head-big-eye.png" title="head"/>
-                <button class="prev-selector">&#9668;</button>
-                <button class="next-selector">&#9658;</button>
-            </div>
-        </div>
-        <div class="middle-row">
-            <div class="left part">
-                <img src="./images/arm-articulated-claw.png" title="left arm"/>
-                <button class="prev-selector">&#9650;</button>
-                <button class="next-selector">&#9660;</button>
-            </div>
-            <div class="center part">
-                <img src="./images/torso-flexible-gauged.png" title="left arm"/>
-                <button class="prev-selector">&#9668;</button>
-                <button class="next-selector">&#9658;</button>
-            </div>
-            <div class="right part">
-                <img src="./images/arm-dual-claw.png" title="left arm"/>
-                <button class="prev-selector">&#9650;</button>
-                <button class="next-selector">&#9660;</button>
-            </div>
-        </div>
-        <div class="bottom-row">
-            <div class="bottom part">
-                <img src="./images/base-single-wheel.png" title="left arm"/>
-                <button class="prev-selector">&#9668;</button>
-                <button class="next-selector">&#9658;</button>
-            </div>
-        </div>
+  <div>
+    <div class="top-row">
+      <div class="top part">
+          <img v-bind:src="availableParts.heads[0].src" title="head"/>
+          <button class="prev-selector">&#9668;</button>
+          <button class="next-selector">&#9658;</button>
+      </div>
     </div>
+    <div class="middle-row">
+      <div class="left part">
+          <img v-bind:src="availableParts.arms[0].src" title="left arm"/>
+          <button class="prev-selector">&#9650;</button>
+          <button class="next-selector">&#9660;</button>
+      </div>
+      <div class="center part">
+          <img v-bind:src="availableParts.torsos[0].src" title="left arm"/>
+          <button class="prev-selector">&#9668;</button>
+          <button class="next-selector">&#9658;</button>
+      </div>
+      <div class="right part">
+          <img v-bind:src="availableParts.arms[0].src" title="left arm"/>
+          <button class="prev-selector">&#9650;</button>
+          <button class="next-selector">&#9660;</button>
+      </div>
+    </div>
+    <div class="bottom-row">
+      <div class="bottom part">
+          <img v-bind:src="availableParts.bases[0].src" title="left arm"/>
+          <button class="prev-selector">&#9668;</button>
+          <button class="next-selector">&#9658;</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import availableParts from '../data/parts';
+
 export default {
-    name: 'RobotBuilder'
-}
+  name: 'RobotBuilder',
+  data() {
+    return {
+      availableParts,
+    };
+  },
+};
 </script>
 
 <style>
@@ -46,7 +53,7 @@ export default {
   width:165px;
   height:165px;
   border: 3px solid #aaa;
-} 
+}
 .part img {
   width:165px;
 }
@@ -109,20 +116,20 @@ export default {
 .left .next-selector {
   top: auto;
   bottom: -28px;
-  left: -3px;    
+  left: -3px;
   width: 144px;
   height: 25px;
 }
 .right .prev-selector {
   top: -28px;
-  left: 24px;  
+  left: 24px;
   width: 144px;
   height: 25px;
 }
 .right .next-selector {
   top: auto;
   bottom: -28px;
-  left: 24px;    
+  left: 24px;
   width: 144px;
   height: 25px;
 }
